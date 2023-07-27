8db4bca220fa4e85bd82d6863483f6a23fe65bef
Khusniddin <isroilovkhusni@gmail.com>
2023-07-27 11:29:22 +0500

    ADD anchor tag

--- HodimBoshqaruv/Models/MockStaffRepository.cs
+++ HodimBoshqaruv/Models/MockStaffRepository.cs
@@ -10,7 +10,7 @@
             _staffs = new List<Staff>()
             {
                 new Staff { Id = 1, FirstName = "Alisher", LastName = "Uzoqov", Email = "alisheruzoqov@gmail.com", Department = "admin"},
-                new Staff { Id= 2, FirstName = "Ulug'bek", LastName ="Rahmatullayev", Email="ulug'bekr@gmail.com", Department ="actor"},
+                new Staff { Id= 2, FirstName = "Ulug'bek", LastName ="Rahmatov", Email="ulug'bekr@gmail.com", Department ="actor"},
                 new Staff { Id= 3, FirstName = "Sardor", LastName ="Rahimxon", Email="sardorrahimxon@gmail.com", Department ="distributor"}
             };
         }
